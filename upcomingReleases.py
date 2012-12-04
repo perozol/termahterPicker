9ca67f135afc55b9ab5e71da78ff52db4d1f236c
Luis.Perozo <perozol@tamu.edu>
2012-12-04 01:17:02 -0600

    script to get new releases from rotten tomatoes.

---
+++ upcomingReleases.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from StringIO import StringIO
+import time
+import re
+
+def getUpcomingReleases():
+    url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?apikey=rnvv7jdrtzez73944vaxu74r'
+    r = requests.get(url)
+
+    host = 'http://imdbapi.org/?'
+    parameters = '&type=json&lang=en-US'
+
+    upcoming = r.json['movies']
+    newReleases = []
+    for movie in upcoming:
+        title = movie['title']
+        query = 'q=' + title.replace(" ","+")
+        r = requests.get(host+query+parameters)
+
+        if ((r.json is not None) and (type(r.json) == dict)):
+            newReleases.append(r.json)
+
+    return newReleases
+
+if __name__=="__main__":
+    start = time.time()
+    print getUpcomingReleases()
+    print "Finished after %d seconds" % (time.time()-start)
+    pass
